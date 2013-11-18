600f3aeae28b92eee48a602ec3775bd309a4215c
Michael Chapman <michchap@cisco.com>
2013-11-18 13:40:43 +1100

    Change repo
    Change repo to CiscoSystems

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='scenariobuilder',
-    version='0.1.3',
+    version='0.1.6',
     packages=['scenariobuilder'],
     license='Apache',
-    url='https://github.com/michaeltchapman/scenariobuilder',
+    url='https://github.com/CiscoSystems/scenariobuilder',
     install_requires=['python-novaclient', 'python-neutronclient', 'PyYaml', 'Jinja2'],
     scripts=['bin/sb'],
     include_package_data=True
